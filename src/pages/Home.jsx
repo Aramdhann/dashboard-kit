4bd3aa37f18255d0ee303b11b10fab756947e009
Aramdhann <radityamochamad@gmail.com>
2024-02-26 23:01:11 +0700

    setting alias vite config

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import SideNavbar from "components/SideNavbar";
 
 const Home = () => {
   return (
-    <div className='text-red-500'>Home</div>
-  )
-}
+    <div>
+      <SideNavbar />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
